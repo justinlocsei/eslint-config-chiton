1ee9c080f54d755f15e90063eb293d42af6e0eec
Justin Locsei <justin.locsei@gmail.com>
2015-12-18 15:07:11 -0500

    Add a React configuration

---
+++ configurations/react.js
@@ -0,0 +1,35 @@
+module.exports = {
+  "extends": "chiton/configurations/es6",
+  "ecmaFeatures": {
+    "jsx": true
+  },
+  "env": {
+    "browser": true
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "jsx-quotes": [2, "prefer-double"],
+    "react/display-name": [2, {"acceptTranspilerName": true}],
+    "react/jsx-boolean-value": [2, "always"],
+    "react/jsx-curly-spacing": [2, "never"],
+    "react/jsx-handler-names": 2,
+    "react/jsx-no-bind": 2,
+    "react/jsx-no-duplicate-props": [2, {"ignoreCase": true}],
+    "react/jsx-no-undef": 2,
+    "react/jsx-pascal-case": 2,
+    "react/jsx-uses-vars": 2,
+    "react/no-danger": 2,
+    "react/no-did-mount-set-state": [2, "allow-in-func"],
+    "react/no-did-update-set-state": [2, "allow-in-func"],
+    "react/no-direct-mutation-state": 2,
+    "react/no-multi-comp": 2,
+    "react/no-unknown-property": 2,
+    "react/prop-types": 2,
+    "react/require-extension": 2,
+    "react/self-closing-comp": 2,
+    "react/wrap-multilines": 2
+  }
+};
+
