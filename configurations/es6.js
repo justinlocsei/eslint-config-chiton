c6adb75f12db5f224d43d47270169e4e41b4dc1b
Justin Locsei <justin.locsei@gmail.com>
2015-12-18 15:07:06 -0500

    Add an ES6 configuration

---
+++ configurations/es6.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "extends": "chiton/configurations/default",
+  "env": {
+    "es6": true
+  },
+  "rules": {
+    "arrow-body-style": [2, "as-needed"],
+    "arrow-parens": [2, "as-needed"],
+    "arrow-spacing": [2, {"after": true, "before": true}],
+    "no-arrow-condition": 2,
+    "no-class-assign": 2,
+    "no-const-assign": 2,
+    "no-dupe-class-members": 2,
+    "no-var": 2,
+    "object-shorthand": [2, "never"],
+    "prefer-const": 2,
+    "prefer-spread": 2,
+    "prefer-template": 2,
+    "require-yield": 2
+  }
+};
+
