a8a4bc039943e8c29b570449a96d78c898b0d4a4
Justin Locsei <justin.locsei@gmail.com>
2015-12-18 15:29:37 -0500

    Remove overlay restrictive checks

--- configurations/node.js
+++ configurations/node.js
@@ -5,11 +5,9 @@ module.exports = {
   },
   "rules": {
     "callback-return": 2,
-    "global-require": 2,
     "handle-callback-err": 2,
     "no-mixed-requires": 2,
     "no-new-require": 2,
-    "no-path-concat": 2,
-    "no-process-env": 2
+    "no-path-concat": 2
   }
 };
