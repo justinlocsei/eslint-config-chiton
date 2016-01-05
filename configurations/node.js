719d1fe8fb2fc65d454df9297c20af4d29800b02
Justin Locsei <justin.locsei@gmail.com>
2016-01-05 14:54:18 -0500

    Allow the use of arrow functions in Node

--- configurations/node.js
+++ configurations/node.js
@@ -3,9 +3,16 @@ module.exports = {
   "env": {
     "node": true
   },
+  "ecmaFeatures": {
+    "arrowFunctions": true
+  },
   "rules": {
+    "arrow-body-style": [2, "as-needed"],
+    "arrow-parens": [2, "as-needed"],
+    "arrow-spacing": [2, {"after": true, "before": true}],
     "callback-return": 2,
     "handle-callback-err": 2,
+    "no-arrow-condition": 2,
     "no-mixed-requires": 2,
     "no-new-require": 2,
     "no-path-concat": 2
