13188e4ef3ca4ede62f6e125c878792e29e1ca00
Justin Locsei <justin.locsei@gmail.com>
2016-03-02 14:23:55 -0500

    Update rule names for eslint 2.0

--- configurations/node.js
+++ configurations/node.js
@@ -12,7 +12,7 @@ module.exports = {
     "arrow-spacing": [2, {"after": true, "before": true}],
     "callback-return": 2,
     "handle-callback-err": 2,
-    "no-arrow-condition": 2,
+    "no-confusing-arrow": 2,
     "no-mixed-requires": 2,
     "no-new-require": 2,
     "no-path-concat": 2
