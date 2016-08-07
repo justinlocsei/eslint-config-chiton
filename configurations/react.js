1aa3c6485e5c6d04480332e64ee848930fcbba70
Justin Locsei <justin.locsei@gmail.com>
2016-08-07 14:52:05 -0400

    Update the named of a renamed React check

--- configurations/react.js
+++ configurations/react.js
@@ -19,14 +19,14 @@ module.exports = {
     "react/jsx-no-undef": 2,
     "react/jsx-pascal-case": 2,
     "react/jsx-uses-vars": 2,
+    "react/jsx-wrap-multilines": 2,
     "react/no-did-mount-set-state": 2,
     "react/no-did-update-set-state": 2,
     "react/no-direct-mutation-state": 2,
     "react/no-multi-comp": 2,
     "react/no-unknown-property": 2,
     "react/prop-types": 2,
-    "react/self-closing-comp": 2,
-    "react/wrap-multilines": 2
+    "react/self-closing-comp": 2
   }
 };
 
